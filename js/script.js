ad1e20e4ee1f8565d5c745bf12bd834916d43d40
Michele Curtaz <mcurtaz@gmail.com>
2020-08-26 15:57:46 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// GOAL: replicare quanto visto a lezione sulla todo-list permettendo all'utente di leggere tutti i task inseriti, inserirne di nuovi, eliminare quelli vecchi
+
+
+// API:
+// 	READ: GET - http://157.230.17.132:porta/todos
+// 	INSERT: POST - http://157.230.17.132:porta/todos - DATA: text
+// 	DELETE: DELETE - http://157.230.17.132:porta/todos/ID
+
+// porta curtaz michele - :3005
+
+$(document).ready(init);
+
+function init(){
+  console.log("hello world!");
+}
